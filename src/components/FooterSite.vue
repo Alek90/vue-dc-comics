63dd9b2ec19313afce6b221b4d15aa71e6c552af
Alek90 <4lek.90@gmail.com>
2021-11-11 17:32:38 +0100

    Creato FooterSite.
    Creato FooterSite e impostata struttura di base.

--- src/components/FooterSite.vue
+++ src/components/FooterSite.vue
@@ -1,3 +1,39 @@
 <template>
-    
+    <footer id="site_footer">
+
+        <div class="footer_main">
+
+            <div class="useful_links">
+
+            </div>
+
+            <div class="pattern_logo">
+
+            </div>
+
+        </div>
+
+        <div class="icons_footer">
+
+            <button class="sing_up">
+                SING-UP NOW
+            </button>
+
+            <div class="social">
+
+            </div>
+
+        </div>
+
+    </footer>
 </template>
+
+<script>
+export default {
+    name: 'FooterSite'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

802f275b7f68046b7bee1a5d38359f50d5705140
Alek90 <4lek.90@gmail.com>
2021-11-11 15:24:32 +0100

    Creato MainSite.vue.
    Creato componente MainSite, crweata struttura interna di base.

---
+++ src/components/MainSite.vue
@@ -0,0 +1,45 @@
+<template>
+    <main id="main_site">
+
+        <!-- provvisorio, da completare/riempire prossimamente -->
+        <div class="jumbotron">
+            <h2>
+                -->Content goes hear|--
+            </h2>
+        </div>
+
+        <!-- menu del corpo della pagina -->
+        <nav class="main_menu">
+
+            <ul>
+                <li>
+                    <img src="" alt="">
+                    <a href=""></a>
+                </li>
+
+                <li>
+                    <img src="" alt="">
+                    <a href=""></a>
+                </li>
+
+                <li>
+                    <img src="" alt="">
+                    <a href=""></a>
+                </li>
+
+                <li>
+                    <img src="" alt="">
+                    <a href=""></a>
+                </li>
+
+                <li>
+                    <img src="" alt="">
+                    <a href=""></a>
+                </li>
+
+            </ul>
+
+        </nav>
+        
+    </main>
+</template>

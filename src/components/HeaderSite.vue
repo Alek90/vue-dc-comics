645e25845751d89ba047fb187131eccc6faac91d
Alek90 <4lek.90@gmail.com>
2021-11-11 12:57:43 +0100

    Creato componente HeaderSite.
    Creato componente, costruita struttura base. Da completare. -Pausa;

---
+++ src/components/HeaderSite.vue
@@ -0,0 +1,21 @@
+<template>
+    <header id="site_header">
+        <div class="logo">
+
+        </div>
+
+        <nav id="site_menu">
+
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderSite'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

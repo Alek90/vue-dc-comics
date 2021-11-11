684b1a0eb43916b09aae476ccb10499d94209c2a
Alek90 <4lek.90@gmail.com>
2021-11-11 14:50:34 +0100

    Inserito style.
    Aggiunte alcune proprietà di stile per vedere l'header su un'unica riga, centrato!

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,10 @@ export default {
 
 <style lang="scss">
 
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
 </style>
--- src/components/HeaderSite.vue
+++ src/components/HeaderSite.vue
@@ -1,21 +1,29 @@
 <template>
     <header id="site_header">
-        <div class="logo">
-            <img src="../assets/img/dc-logo.png" alt="">
+
+        <div class="container">
+            
+            <!-- logo del brand -->
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="">
+            </div>
+
+            <!-- menu di navigazione del sito -->
+            <nav id="site_menu">
+                <a href="">CHARACTERS</a>
+                <a href="">COMICS</a>
+                <a href="">MOVIES</a>
+                <a href="">TV</a>
+                <a href="">GAMES</a>
+                <a href="">COLLECTIBLES</a>
+                <a href="">VIDEOS</a>
+                <a href="">FANS</a>
+                <a href="">NEWS</a>
+                <a href="">SHOP</a>
+            </nav>
+
         </div>
 
-        <nav id="site_menu">
-            <a href="">CHARACTERS</a>
-            <a href="">COMICS</a>
-            <a href="">MOVIES</a>
-            <a href="">TV</a>
-            <a href="">GAMES</a>
-            <a href="">COLLECTIBLES</a>
-            <a href="">VIDEOS</a>
-            <a href="">FANS</a>
-            <a href="">NEWS</a>
-            <a href="">SHOP</a>
-        </nav>
     </header>
 </template>
 
@@ -26,5 +34,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.container{
+    width: 70%;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
 
 </style>

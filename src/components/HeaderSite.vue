314d403531629937f73216de367b8ee9a26d0cd2
Alek90 <4lek.90@gmail.com>
2021-11-11 14:36:58 +0100

    Aggiunti link nel nav.
    Da realizzare lo style.

--- src/components/HeaderSite.vue
+++ src/components/HeaderSite.vue
@@ -5,7 +5,16 @@
         </div>
 
         <nav id="site_menu">
-
+            <a href="">CHARACTERS</a>
+            <a href="">COMICS</a>
+            <a href="">MOVIES</a>
+            <a href="">TV</a>
+            <a href="">GAMES</a>
+            <a href="">COLLECTIBLES</a>
+            <a href="">VIDEOS</a>
+            <a href="">FANS</a>
+            <a href="">NEWS</a>
+            <a href="">SHOP</a>
         </nav>
     </header>
 </template>
